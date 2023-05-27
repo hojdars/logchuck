46cf4621cd793fdc2d11925f044eec029e4f8a1a
Stepan Hojdar <hojdar.stepan@gmail.com>
2023-05-27 10:09:42 +0200

    Add support for comma-separated microseconds, improve error reporting

--- src/timestamp.rs
+++ src/timestamp.rs
@@ -5,36 +5,59 @@ use chrono::{DateTime, FixedOffset};
 use std::fmt;
 
 #[derive(Debug, Clone)]
-pub struct LineError;
+pub struct LineError {
+    pub error_message: String,
+}
 
 impl fmt::Display for LineError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "line does not have a timestamp")
+        write!(f, "{}", self.error_message)
     }
 }
 
-pub fn parse_timestamp_utc(time: &str) -> Result<DateTime<FixedOffset>, LineError> {
-    let mut time_utc: String = time.to_string();
-    time_utc.push_str(" +0000");
-    match DateTime::parse_from_str(time_utc.as_str(), "%Y-%m-%d %H:%M:%S%.f %z") {
-        Ok(date_time) => Ok(date_time),
-        Err(_) => Err(LineError {}),
-    }
-}
-
-pub fn get_timestamp_from_line(line: &str) -> Result<String, LineError> {
+pub fn get_timestamp_from_line(line: &str) -> Result<DateTime<FixedOffset>, LineError> {
     let mut chunks = line.split(' ');
     let mut timestamp: String = String::new();
 
     match chunks.next() {
         Some(text) => timestamp.push_str(text),
-        None => return Err(LineError {}),
+        None => {
+            return Err(LineError {
+                error_message: String::from("line does not have a date"),
+            })
+        }
     }
     timestamp.push(' ');
     match chunks.next() {
         Some(text) => timestamp.push_str(text),
-        None => return Err(LineError {}),
+        None => {
+            return Err(LineError {
+                error_message: String::from("line does not have a time"),
+            })
+        }
     }
 
-    Ok(timestamp)
+    return parse_timestamp_utc(timestamp.as_str());
+}
+
+fn parse_timestamp_utc(time: &str) -> Result<DateTime<FixedOffset>, LineError> {
+    let mut time_utc: String = time.to_string();
+    time_utc.push_str(" +0000");
+
+    if let Ok(date_time) = DateTime::parse_from_str(time_utc.as_str(), "%Y-%m-%d %H:%M:%S%.f %z") {
+        return Ok(date_time);
+    }
+
+    if let Ok(date_time) = DateTime::parse_from_str(time_utc.as_str(), "%Y-%m-%d %H:%M:%S%.f %z") {
+        return Ok(date_time);
+    }
+
+    time_utc = time_utc.replace(',', ".");
+    if let Ok(date_time) = DateTime::parse_from_str(time_utc.as_str(), "%Y-%m-%d %H:%M:%S%.f %z") {
+        return Ok(date_time);
+    }
+
+    Err(LineError {
+        error_message: String::from("cannot parse timestamp"),
+    })
 }
